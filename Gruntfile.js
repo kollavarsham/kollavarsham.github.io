95a2bdd604cf3a21766c84f630b1b93d79821d05
Hari Menon <me@harimenon.com>
2014-01-23 09:52:34 -0500

    Changed the watch server host to 0.0.0.0

--- Gruntfile.js
+++ Gruntfile.js
@@ -62,8 +62,8 @@ module.exports = function (grunt) {
             options: {
                 port: 9000,
                 livereload: 35729,
-                // Change this to '0.0.0.0' to access the server from outside
-                hostname: 'localhost'
+                // Change this to 'localhost' to prevent accessing the server from outside
+                hostname: '0.0.0.0'
             },
             livereload: {
                 options: {
